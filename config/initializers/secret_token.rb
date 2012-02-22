bd7060621c84d164d9038f68c2680266fab8f3c5
Nicholas Audo <naudo@elctech.com>
2012-02-22 11:06:31 -0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoalIe::Application.config.secret_token = '5a8adbfe1fd5bca011b41055bb767a816fb5412c47efc0b5f471fd437ba48272a48fe2767ed76116ae64e0c06fd865361e50be2b77488e4db7167e67a38486af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GoalIe::Application.config.session_store :cookie_store, key: '_goal.ie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GoalIe::Application.config.session_store :active_record_store
